fd7261fef3a774bef7a1085ba4d8c08e72ec6021
philippzeppelin <philippzeppelin@gmail.com>
2023-01-14 14:51:31 +0300

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Philipp Zeppelin on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
